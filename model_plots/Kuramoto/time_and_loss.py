5b96002b31c16a8d1e6b8418f8a6e4c00e6fea15
Thomas Gaskin <tgaskin@live.co.uk>
2023-03-14 14:59:17 +0000

    Add GPU compute times
    Remove old configuration sets
    
    Remove .csv saving from Computational performance config

--- model_plots/Kuramoto/time_and_loss.py
+++ model_plots/Kuramoto/time_and_loss.py
@@ -1,11 +1,6 @@
 import logging
-from typing import Union
 
-import matplotlib.lines as mlines
-import matplotlib.pyplot as plt
-import scipy.ndimage
 import xarray as xr
-from dantro.plot.funcs.generic import errorbars
 
 from utopya.eval import PlotHelper, is_plot_func
 
@@ -15,18 +10,22 @@ log = logging.getLogger(__name__)
 @is_plot_func(
     use_dag=True,
     use_helper=True,
-    required_dag_tags=(
-        "loss_data",
-        "time_data",
-        "regression_time"
-    ),
+    required_dag_tags=("loss_data", "CPU_time", "GPU_time", "regression_time"),
 )
-def time_and_loss(hlpr: PlotHelper, *, data: dict, loss_color: str, time_color: str, regression_color: str):
-    """ Plots a comparison of time and loss values onto two axes """
+def time_and_loss(
+    hlpr: PlotHelper,
+    *,
+    data: dict,
+    loss_color: str,
+    CPU_color: str,
+    GPU_color: str,
+    regression_color: str
+):
+    """Plots a comparison of time and loss values onto two axes"""
     loss_data: xr.Dataset = data["loss_data"]
-    time_data: xr.Dataset = data["time_data"]
+    CPU_time: xr.Dataset = data["CPU_time"]
+    GPU_time: xr.Dataset = data["GPU_time"]
     regression_time: xr.Dataset = data["regression_time"]
-
     ax1 = hlpr.ax
 
     # Plot the loss data
@@ -44,13 +43,24 @@ def time_and_loss(hlpr: PlotHelper, *, data: dict, loss_color: str, time_color: 
     ax2.set_ylim([0.1, 0.5])
 
     hlpr.select_axis(ax=ax1)
-    time_data["y"].plot(color=time_color)
+    CPU_time["y"].plot(color=CPU_color)
     ax1.fill_between(
-        time_data.coords["N"],
-        (time_data["y"] + time_data["yerr"]),
-        (time_data["y"] - time_data["yerr"]),
+        CPU_time.coords["N"],
+        (CPU_time["y"] + CPU_time["yerr"]),
+        (CPU_time["y"] - CPU_time["yerr"]),
         alpha=0.5,
-        color=time_color,
+        color=CPU_color,
         lw=0,
     )
-    regression_time.plot(color=regression_color, ax=ax1, linestyle='dotted')
+
+    GPU_time["y"].plot(color=GPU_color)
+    ax1.fill_between(
+        GPU_time.coords["N"],
+        (GPU_time["y"] + GPU_time["yerr"]),
+        (GPU_time["y"] - GPU_time["yerr"]),
+        alpha=0.5,
+        color=GPU_color,
+        lw=0,
+    )
+
+    regression_time.plot(color=regression_color, ax=ax1, linestyle="dotted")
