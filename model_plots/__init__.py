38bae04a67423f74fda115478fa6f288e783f915
Thomas Gaskin <tgaskin@live.co.uk>
2023-03-17 13:38:52 +0000

    Corrections to SIR and Kuramoto base plots

--- model_plots/__init__.py
+++ model_plots/__init__.py
@@ -22,6 +22,8 @@ register_operation(name="pd.Index", func=pd.Index)
 register_operation(name="np.maximum", func=np.maximum)
 register_operation(name="np.subtract", func=np.subtract)
 register_operation(name="np.exp", func=np.exp)
+register_operation(name="np.sin", func=np.sin)
+register_operation(name="xr.where", func=lambda d, *a, **k: d.where(*a, **k))
 register_operation(name=".idxmax", func=lambda d, *a, **k: d.idxmax(*a, **k))
 register_operation(name=".stack", func=lambda d, *a, **k: d.stack(*a, **k))
 from .data_ops import *
