a404f34ff23bc18b88685c17d94b75536763a11b
Thomas Gaskin <tgaskin@live.co.uk>
2023-12-12 18:20:01 +0000

    Corrections to SIR plots and renaming of configuration sets

--- model_plots/SIR/trajectories_from_densities.py
+++ model_plots/SIR/trajectories_from_densities.py
@@ -1,10 +1,11 @@
 import logging
-
+from tqdm import trange
+from tqdm.contrib.logging import logging_redirect_tqdm
 import numpy as np
 import torch
 import xarray as xr
 
-import models.SIR
+from models.SIR import generate_smooth_data
 from utopya.eval import is_operation
 
 log = logging.getLogger(__name__)
@@ -15,7 +16,7 @@ def _adjust_for_time_dependency(
 ) -> dict:
     """Adjusts the parameter configuration for time dependent parameters, if given."""
 
-    time_dependent_parameters = cfg["Data"].get("time_dependent_params", {})
+    time_dependent_parameters = cfg["Data"].get("time_dependent_parameters", {})
 
     # Extend any time-dependent parameters, if necessary
     for param in time_dependent_parameters.keys():
@@ -34,21 +35,20 @@ def _adjust_for_time_dependency(
     return param_cfg
 
 
-def _densities_from_joint(
+@is_operation("SIR_densities_from_joint")
+def densities_from_joint(
     joint: xr.Dataset,
-    generating_func,
     *,
     true_counts: xr.Dataset,
     cfg: dict,
-):
-    """Runs the model with the estimated parameters, given in the xr.Dataset, and weights each time series with
+) -> xr.Dataset:
+    """Runs the SIR ODE model with the estimated parameters, given in the xr.Dataset, and weights each time series with
     its corresponding probability. The probabilities must be normalised to 1.
 
     :param joint: the ``xr.Dataset`` of the joint parameter distribution
-    :param generating_func: the generating function used to run the model
     :param true_counts: the xr.Dataset of true counts
     :param cfg: the run configuration of the original data (only needed if parameters are time dependent)
-    :return: an xr.Dataset of the mean, mode, std, and true densities for all compartments
+    :return: an ``xr.Dataset`` of the mean, mode, std, and true densities for all compartments
     """
 
     res = []
@@ -65,41 +65,44 @@ def _densities_from_joint(
 
     sample_cfg = cfg["Data"]["synthetic_data"]
 
-    for s in range(len(joint.coords["sample"])):
-        # Get the sample
-        sample = joint.isel({"sample": [s]})
+    with logging_redirect_tqdm():
 
-        # Construct the configuration, taking time-dependent parameters into account
-        sample = sample.unstack("sample")
+        for s in (pbar := trange(len(joint.coords["sample"]))):
+            pbar.set_description(f"Drawing {len(joint.coords['sample'])} samples from joint distribution: ")
 
-        sample_cfg.update({key: val.item() for key, val in sample.coords.items()})
-        param_cfg = _adjust_for_time_dependency(sample_cfg, cfg, true_counts)
+            # Get the sample
+            sample = joint.isel({"sample": [s]})
 
-        # Generate smooth data
-        generated_data = generating_func(
-            cfg=param_cfg,
-            num_steps=len(true_counts.coords["time"]) - 1,
-            dt=cfg["Data"]["synthetic_data"].get("dt", None),
-            k_q=cfg["Data"]["synthetic_data"].get("k_q", None),
-            write_init_state=True,
-            init_state=torch.from_numpy(
-                true_counts.isel({"time": 0}, drop=True).data
-            ).float(),
-        ).numpy()
+            # Construct the configuration, taking time-dependent parameters into account
+            sample = sample.unstack("sample")
 
-        res.append(
-            xr.DataArray(
-                data=[[generated_data]],
-                dims=["sample", "type", "time", "kind", "dim_name__0"],
-                coords=dict(
-                    sample=[s],
-                    type=["mean prediction"],
-                    time=true_counts.coords["time"],
-                    kind=true_counts.coords["kind"],
-                    dim_name__0=true_counts.coords["dim_name__0"],
-                ),
+            sample_cfg.update({key: val.item() for key, val in sample.coords.items()})
+            param_cfg = _adjust_for_time_dependency(sample_cfg, cfg, true_counts)
+
+            # Generate smooth data
+            generated_data = generate_smooth_data(
+                cfg=param_cfg,
+                num_steps=len(true_counts.coords["time"]) - 1,
+                dt=cfg["Data"]["synthetic_data"].get("dt", None),
+                init_state=torch.from_numpy(
+                    true_counts.isel({"time": 0}, drop=True).data
+                ).float(),
+                write_init_state=True,
+            ).numpy()
+
+            res.append(
+                xr.DataArray(
+                    data=[[generated_data]],
+                    dims=["sample", "type", "time", "kind", "dim_name__0"],
+                    coords=dict(
+                        sample=[s],
+                        type=["mean prediction"],
+                        time=true_counts.coords["time"],
+                        kind=true_counts.coords["kind"],
+                        dim_name__0=true_counts.coords["dim_name__0"],
+                    ),
+                )
             )
-        )
 
     # Concatenate all the time series
     res = xr.concat(res, dim="sample").squeeze(["dim_name__0"], drop=True)
@@ -110,15 +113,14 @@ def _densities_from_joint(
 
     # Perform a run using the mode
     mode_params = _adjust_for_time_dependency(sample_cfg, cfg, true_counts)
-    mode_data = generating_func(
+    mode_data = generate_smooth_data(
         cfg=mode_params,
         num_steps=len(true_counts.coords["time"]) - 1,
         dt=cfg["Data"]["synthetic_data"].get("dt", None),
-        k_q=cfg["Data"]["synthetic_data"].get("k_q", None),
-        write_init_state=True,
         init_state=torch.from_numpy(
             true_counts.isel({"time": 0}, drop=True).data
         ).float(),
+        write_init_state=True,
     ).numpy()
 
     mode_data = xr.DataArray(
@@ -153,15 +155,3 @@ def _densities_from_joint(
     data = xr.Dataset(data_vars=dict(mean=mean, std=std))
 
     return data
-
-
-@is_operation("SIR_densities_from_joint")
-def SIR_densities_from_joint(
-    joint: xr.Dataset,
-    *,
-    true_counts: xr.Dataset,
-    cfg: dict,
-) -> xr.Dataset:
-    return _densities_from_joint(
-        joint, models.SIR.generate_smooth_data, true_counts=true_counts, cfg=cfg
-    )
