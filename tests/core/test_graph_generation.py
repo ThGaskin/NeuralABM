fa720cf45a6083be4a1fc9aa611ba38bc85fe691
Thomas Gaskin <tgaskin@live.co.uk>
2023-12-12 22:56:24 +0000

    Expand graph_generation test coverage

--- tests/core/test_graph_generation.py
+++ tests/core/test_graph_generation.py
@@ -1,10 +1,16 @@
 import sys
 from os.path import dirname as up
+from builtins import *
+import pytest
 
 import networkx as nx
+import dantro
+from dantro.containers import XrDataContainer
+import numpy as np
+
 from dantro._import_tools import import_module_from_path
 from pkg_resources import resource_filename
-
+import h5py as h5
 from utopya.yaml import load_yml
 
 sys.path.insert(0, up(up(up(__file__))))
@@ -20,7 +26,20 @@ test_cfg = load_yml(CFG_FILENAME)
 
 def test_graph_generation():
     for _, config in test_cfg.items():
+
+        _raises = config.pop("_raises", False)
+        _exp_exc = Exception if not isinstance(_raises, str) else globals()[_raises]
+        _warns = config.pop("_warns", False)
+        _exp_warning = UserWarning if not isinstance(_warns, str) else globals()[_warns]
+        _match = config.pop("_match", " ")
+
         nw_cfg = config.pop("network")
+
+        if _raises:
+            with pytest.raises(_exp_exc, match=_match):
+                graph_gen.generate_graph(**config, **nw_cfg)
+            continue
+
         # Generate the graph from the configuration
         G = graph_gen.generate_graph(**config, **nw_cfg)
 
@@ -41,3 +60,54 @@ def test_graph_generation():
             assert not G.is_directed()
         elif nw_cfg["type"] == "BollobasRiordan":
             assert G.is_directed()
+
+def test_graph_saving(tmpdir):
+
+    # Create an h5File in the temporary directory for the
+    h5dir = tmpdir.mkdir("hdf5_data")
+    h5file = h5.File(h5dir.join(f"test_file.h5"), "w")
+    nw_group = h5file.create_group("graph")
+    nw_group.attrs["content"] = "graph"
+    nw_group.attrs["allows_parallel"] = False
+    nw_group.attrs["is_directed"] = True
+
+    # Test saving a static graph
+    G1 = graph_gen.generate_graph(N=16, mean_degree=4, type='random', graph_props=dict(is_directed=True, is_weighted=True))
+
+    graph_gen.save_nw(G1, nw_group, write_adjacency_matrix=True)
+
+    # Load the network
+    GG = dantro.groups.GraphGroup(
+        name="true_network",
+        attrs=dict(
+            directed=h5file["graph"].attrs["is_directed"],
+            parallel=h5file["graph"].attrs["allows_parallel"],
+        ),
+    )
+    GG.new_container(
+        "nodes",
+        Cls=XrDataContainer,
+        data=np.array(h5file["graph"]["_vertices"]),
+    )
+    GG.new_container("edges", Cls=XrDataContainer, data=[])
+
+    edges = np.array(h5file["graph"]["_edges"])
+    edge_weights = np.expand_dims(
+        np.array(h5file["graph"]["_edge_weights"]), -1
+    )
+    weighted_edges = np.squeeze(np.concatenate((edges, edge_weights), axis=-1))
+
+    A = np.array(h5file["graph"]["_adjacency_matrix"])
+
+    G2 = GG.create_graph()
+    G2.add_weighted_edges_from(weighted_edges, "weight")
+    h5file.close()
+
+    A1 = nx.adjacency_matrix(G1).toarray()
+    A2 = nx.adjacency_matrix(G2).toarray()
+
+    # Assert the adjacency matrices are all equal
+    assert (np.all([a == pytest.approx(0, abs=1e-6) for a in A1-A2]))
+    assert (np.all([a == pytest.approx(0, abs=1e-6) for a in A1-A]))
+
+
